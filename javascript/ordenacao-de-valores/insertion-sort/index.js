bd81e144eef26c1b3ddc70f27e681edb80ecbfa4
Israel Hamdan <IsraelSantos_20k@outlook.com>
2023-09-24 17:06:54 -0300

    estudando o quicksort

--- javascript/ordenacao-de-valores/insertion-sort/index.js
+++ javascript/ordenacao-de-valores/insertion-sort/index.js
@@ -1,3 +1,41 @@
-let numbers = [];
+let numbersUnorded = [];
 
-function insertionSort(numbers) {}
+function action() {
+  const getBtn = document.getElementById("btn");
+  getBtn.addEventListener("click", () => {
+    iteracao(numbersUnorded);
+  });
+
+  const getBtnOrder = document.getElementById("btnOrder");
+  getBtnOrder.addEventListener("click", () => {
+    insertionSort(numbersUnorded);
+  });
+}
+action();
+
+function insertionSort(numbersUnorded) {
+  let current;
+  for (let i = 1; i < vetor.length; i++) {
+    let j = i - 1;
+    current = vetor[i];
+    while (j >= 0 && current < vetor[j]) {
+      numbersUnorded[j + 1] = numbersUnorded[j];
+      j--;
+    }
+    numbersUnorded[j + 1] = atual;
+  }
+  const numbersOrderd = [...numbersUnorded];
+  console.log(numbersOrderd);
+  return numbersOrderd;
+}
+
+function iteracao(numbersUnorded) {
+  const getValue = parseInt(document.getElementById("value").value);
+  const getBtn = document.getElementById("btn");
+
+  getBtn.addEventListener("click", () => {
+    if (!isNaN(getValue)) {
+      numbersUnorded.push(getValue);
+    }
+  });
+}
